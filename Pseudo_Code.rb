996c02cf43319270804d50fdab0037890b3cec9b
NvHCode <nielsvanhensbergen@outlook.com>
2022-06-22 13:51:47 +0900

    attempt at pseudo code

---
+++ Pseudo_Code.rb
@@ -0,0 +1,24 @@
+# Formal Pseudo-code:
+# 1
+# A method that returns the sum of two integers
+# - define a method which:
+# - takes two integers
+# - Take the sum of two integers 
+# - returns the sum of two integers 
+# END
+
+#2
+# a method that takes an array of strings, 
+# and returns a single string
+# concatenated together
+# - define a method which:
+# SET an Array of strings
+# PRINt a concatenated string
+# END 
+
+#3
+# SET a collection of integers called "numbers"
+# SET new array with every other element from the origanal array
+# PRINT new arr
+
+everyOther([1,4,7,2,5]) # => [1,7,5]
